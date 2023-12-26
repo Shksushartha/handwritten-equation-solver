477737dd9f4fd3ac87fec326b336b5c90c4a4700
Sushartha Shakya <92314523+Shksushartha@users.noreply.github.com>
2023-12-26 07:20:48 +0545

    Update CheckEquationType.py

--- CheckEquationType.py
+++ CheckEquationType.py
@@ -1,6 +1,6 @@
 import re
 def getEquationType(equation):
-    linear_pattern = r'^\s*([+-]?\s*\d*\s*[a-zA-Z]?)\s*([+-]\s*\d*\s*[a-zA-Z]?)*\s*=\s*(\s*[+-]?\s*\d*\s*[a-zA-Z]?)\s*([+-]\s*\d*\s*[a-zA-Z]?)*$'
+    linear_pattern = r'^(\s*[+-]?\s*\d*\s*[a-zA-Z])\s*(\s*[+-]\s*\d*\s*[a-zA-Z]\s*)*\s*=\s*([+-]?\s*\d*\s*)\s*$'
 
     # Check if the input matches the linear equation pattern
     if re.match(linear_pattern, equation):
