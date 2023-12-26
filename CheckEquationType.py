76b01ded1b505b12c612c43e544e2f7f49be7bd8
Sushartha Shakya <92314523+Shksushartha@users.noreply.github.com>
2023-12-26 07:29:33 +0545

    Update CheckEquationType.py
    quadratic equation recognition

--- CheckEquationType.py
+++ CheckEquationType.py
@@ -2,8 +2,12 @@ import re
 def getEquationType(equation):
     linear_pattern = r'^(\s*[+-]?\s*\d*\s*[a-zA-Z])\s*(\s*[+-]\s*\d*\s*[a-zA-Z]\s*)*\s*=\s*([+-]?\s*\d*\s*)\s*$'
 
+    quadratic_pattern = r'^(\s*[+-]?\s*(\d*)\s*([a-zA-Z])\s*(\^\s*(\d+))?)\s*(\s*[+-]\s*(\d*)\s*([a-zA-Z])\s*(\^\s*(\d+))?)*\s*=\s*([+-]?\s*\d*\s*)\s*$'
+
     # Check if the input matches the linear equation pattern
     if re.match(linear_pattern, equation):
         return 1 #linearEquation
+    elif re.match(quadratic_pattern, equation):
+        return 2 #linearEquation
     else:
         return 0
